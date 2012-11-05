74d82e150c6de7583700e5a4021f19b570221402
pasberth <pasberth@gmail.com>
2012-11-05 14:18:03 +0900

    timemachine

--- Quasar.rb
+++ Quasar.rb
@@ -10,20 +10,45 @@ module Quasar
     @darkmatter ||= DarkMatter.new
   end
 
+
   class Universe
 
     BIG_CRUNCH = -> x { x }
 
     def initialize eras = []
-      @value = eras.reverse_each.inject(BIG_CRUNCH) do |nxt, era|
-        -> x { nxt.call(era.call(x)) }
-      end
+      @eras = eras
+      @value = build_value(eras)
     end
 
     def value a
       @value.call(a)
     end
+
+    def back_to at
+      Universe.new(@eras[at..-1])
+    end
+
+    private
+
+      def build_value eras = @eras
+        eras.reverse_each.inject(BIG_CRUNCH) do |nxt, era|
+          -> x { nxt.call(era.call(x)) }
+        end
+      end
   end
+
+
+  class Timemachine
+
+    def initialize universe
+      @universe = universe
+    end
+
+    def back_to at
+      @universe.back_to(at)
+    end
+  end
+
 
   class Blackhole < BasicObject
 
@@ -45,10 +70,20 @@ end
 
 # example
 include Quasar
-anUniverse = Universe.new([ -> x { p x },
-                            -> y { p y + 1 },
-                            -> z { p z + 2 }
+
+anUniverse = Universe.new([ -> x { p x + 1 },
+                            -> y { p y + 2 },
+                            -> z { p z + 3 }
                           ])
 anUniverse.value(0)
+# 1
+# 3
+# 6
+
+timemachine = Timemachine.new(anUniverse)
+timemachine.back_to(1).value(0)
+# 2
+# 5
+
 puts blackhole.x.y.z
 puts Quasar.blackhole.x.y.z
